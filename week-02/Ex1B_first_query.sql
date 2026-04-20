010da93ec3cca6afa25f5da0fe3ac1200afd0a26
Ceyhun Ugur <ceyhun.ugur.19@hotmail.com>
2026-04-19 22:27:39 -0500

    git commit -m "added comment to file"

---
+++ week-02/Ex1B_first_query.sql
@@ -0,0 +1,8 @@
+/*
+	Sakila is a database used to collect information about film makers, actors, and films.
+*/
+
+USE sakila;
+SELECT * FROM actor;  -- Retrieved 200 rows of records.
+
+SELECT * FROM film;  -- Retrieved 1000 rows of records.
