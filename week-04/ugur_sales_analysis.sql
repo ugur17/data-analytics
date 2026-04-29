07bbb6826b422468d038b36a09bef1733407dc72
Ceyhun Ugur <ceyhun.ugur.19@hotmail.com>
2026-04-29 01:10:56 -0500

    new queries solved

--- week-04/ugur_sales_analysis.sql
+++ week-04/ugur_sales_analysis.sql
@@ -26,7 +26,7 @@ where bottle_qty > 12;
 -- keyword (e.g., 'Limited', 'Spiced'). What categories do they belong to?
 -- (Opportunity: Identifying niche product variants).
 
-select distinct category_name
+select item_description, category_name
 from products
 where item_description like '%Irish%';
 
@@ -79,6 +79,15 @@ limit 1;
 -- sorted from highest to lowest?
 -- (Threat: Identifying your top competitors in that space).
 
+-- NOTE: I've changed the perspective from comparing retail partners to comparing 
+-- categories.
+
+select p.category_name, sum(total)
+from products p
+join sales s on p.item_no = s.item
+where vendor_name = 'Jim Beam Brands'
+group by p.category_name
+order by sum(total) desc;
 
 
 -- 8. Which stores had total sales revenue for your [Category/Vendor] exceeding $2,000?
@@ -91,3 +100,90 @@ join stores st on s.store = st.store
 group by st.name
 having sum(total) >= 2000
 order by total_revenue desc;
+
+
+-- Joins
+-- 9. Find all sales records where the category_name is either your chosen category
+-- or a closely related competitor category (e.g., 'VODKA 80 PROOF' vs 'IMPORTED VODKA').
+-- (Threat: Comparing performance against direct substitutes).
+
+
+
+-- 10. List all transactions where the state bottle cost was between $10 and $20 for
+-- your [Category/Vendor].
+-- (Opportunity: Analyzing performance in the "mid-tier" price bracket).
+
+select * 
+from sales s
+join products p on p.item_no = s.item
+where vendor_name = 'Jim Beam Brands'
+	and state_btl_cost::numeric between 10 and 20;
+
+
+-- 11. Write a query that displays each store's total sales for your [Category/Vendor]
+-- along with the store's name and address from the stores_table.
+-- (Strength: Mapping your physical sales footprint).
+
+select st.name, store_address, sum(total) as total_sales
+from sales s
+join stores st on s.store = st.store
+join products p on p.item_no = s.item
+where vendor_name = 'Jim Beam Brands'
+group by st.name, store_address
+order by 3 desc;
+
+
+-- 12. For each sale in your [Category], display the transaction date, total amount,
+-- and the population of the county where the sale occurred by joining with counties_
+-- (Opportunity: Correlating sales volume with population density).table.
+
+select * from counties;
+
+select date, total, population
+from sales s
+join counties c on s.county = c.county
+join products p on p.item_no = s.item
+where vendor_name = 'Jim Beam Brands';
+
+-- 13. Write a query that shows total sales for your [Category/Vendor] by county.
+-- Which county generates the most revenue for you?
+-- (Strength: Identifying your geographic stronghold).
+
+select s.county, sum(total)
+from sales s
+join counties c on s.county = c.county
+join products p on p.item_no = s.item
+where vendor_name = 'Jim Beam Brands'
+group by s.county
+order by 2 desc ;
+
+-- 14. Join the sales_table and products_table to show total revenue for your [Vendor]
+-- alongside the proof and pack size of the items.
+-- (Strength: Determining if higher proof or larger packs drive more revenue).
+
+select * from products;
+
+select item_no, item_description, bottle_size, proof, sum(total)
+from sales s
+join products p on p.item_no = s.item
+where vendor_name = 'Jim Beam Brands'
+group by item_no
+order by sum(total) desc;
+
+-- 15. Are there any counties in the counties_table that have a population over 10,000
+-- but zero sales for your [Category/Vendor]?
+-- (Opportunity: Identifying untapped markets).
+
+select s.county, sum(total)
+from sales s
+join counties c on s.county = c.county
+join products p on p.item_no = s.item
+where vendor_name = 'Jim Beam Brands'
+	and population > 10000
+group by s.county
+order by 2 asc ;
+
+
+-- 16. Display total revenue for your [Category/Vendor] grouped by the store status_
+-- (from stores_table). Are active stores ('A') performing significantly better than others?
+-- (Threat: Assessing the risk of sales tied to inactive or closed locations).
