0acb665a037387765ec1c24e89e68156819c1475
Ceyhun Ugur <ceyhun.ugur.19@hotmail.com>
2026-05-05 13:25:09 -0500

    add introductory Python script with basic print statements and variable usage

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,18 @@
 print('Hello world!')
+
+
+message = "Hello world!"
+print(message)
+
+# “Hello world!” printed twice because we are printing in the print statement
+# and also printing the variable message which contains the same string.
+
+
+# Displaying dollars and cents
+dollars = 3
+cents = .50
+print(dollars + cents)
+
+d_str = "3"
+c_str = "50"
+print(d_str + ' ' + c_str)
