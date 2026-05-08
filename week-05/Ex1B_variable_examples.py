949a5093586bcf8a2f34bfc373545b3c44ae4c3a
Ceyhun Ugur <ceyhun.ugur.19@hotmail.com>
2026-05-08 10:49:57 -0500

    ex1b completed.

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,21 @@
+customer_id = 1 # should be auto incremented in a real database
+customer_name = 'Ceyhun' #should be string
+customer_gender = 'M' #should be string
+customer_dob = '03/27/1999' #should be string
+customer_dl_no = 'D1234567' #should be string
+customer_auto_policy_no = 'P1234567' #should be string
+
+
+#False	def	if	raise
+#None	del	import	return
+#True	elif	in	try
+#and	else	is	while
+#as	except	lambda	with
+#assert	finally	nonlocal	yield
+#break	for	not
+#class	form	or
+#continue	global	pass
+
+# if else for if-else blocks
+#True-False for boolean values
+#for while for loops
