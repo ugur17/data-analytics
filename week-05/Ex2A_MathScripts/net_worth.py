fee42303cfa7cd41068705b6b35773a1ed360be9
Ceyhun Ugur <ceyhun.ugur.19@hotmail.com>
2026-05-09 21:14:38 -0500

    made some calculations

---
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,10 @@
+# Example asset values (cash, investments, property, vehicles, etc.)
+assets = 45000  # total value of assets
+
+debts = 17500  # total amount owed in debts
+
+net_worth = assets - debts
+
+print(f"Your total assets are {assets}")
+print(f"Your total debts are {debts}")
+print(f"Your net worth is {net_worth}")
